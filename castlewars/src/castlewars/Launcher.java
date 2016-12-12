77325e92aaa714129769201d5d0c4716f2c8788d
Jakub <Jakub@DESKTOP-CUMAHIR>
2016-12-12 15:44:50 +0100

    initial commit, created Launcher

---
+++ castlewars/src/castlewars/Launcher.java
@@ -0,0 +1,18 @@
+package castlewars;
+
+/**
+ *
+ * @author Jakub
+ */
+public class Launcher {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //connect to database
+        //load from database
+        //call login "Presenter"
+    }
+    
+}
