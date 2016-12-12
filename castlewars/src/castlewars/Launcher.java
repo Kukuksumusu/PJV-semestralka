613291e4238673069c65eb59d09b2618cdf7b7c1
Kukuksumusu <stercjak@fit.cvut.cz>
2016-12-12 19:51:23 +0100

    some initial tries

--- castlewars/src/castlewars/Launcher.java
+++ castlewars/src/castlewars/Launcher.java
@@ -1,10 +1,14 @@
 package castlewars;
 
+import castlewars.scenes.ProfileSelectSceneBuilder;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
 /**
  *
  * @author Jakub
  */
-public class Launcher {
+public class Launcher extends Application {
 
     /**
      * @param args the command line arguments
@@ -13,6 +17,14 @@ public class Launcher {
         //connect to database
         //load from database
         //call login "Presenter"
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        ProfileSelectSceneBuilder pssb = new ProfileSelectSceneBuilder();
+        stage.setScene(pssb.buildScene());
+        stage.show();
     }
     
 }
---
+++ castlewars/src/castlewars/scenes/BaseSceneBuilder.java
@@ -0,0 +1,13 @@
+package castlewars.scenes;
+
+import javafx.scene.Scene;
+
+/**
+ *
+ * @author Kukuksumusu
+ */
+public abstract class BaseSceneBuilder {
+    
+    public abstract Scene buildScene() throws Exception;
+    
+}
